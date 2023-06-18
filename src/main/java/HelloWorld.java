2b644a389a5552a4550c372c4ff5db6e483033e7
yennhc <yen.nguyenhoacat@gmail.com>
2023-06-18 23:08:33 +0700

    a

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Tin Hoc That La Don Gian5555555555");
+		get("/hello", (req, res) -> "Hello Yen");
 	}
 }
